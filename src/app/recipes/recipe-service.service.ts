22e411a23ff61c3d50f70fd0977d1c73ba858c48
mostafa <mostafahamed73@yahoo.com>
2019-04-23 09:08:47 +0200

    add ingredient to recipe list

--- src/app/recipes/recipe-service.service.ts
+++ src/app/recipes/recipe-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,22 @@ export class RecipeServiceService {
 
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe ('A Test recipe', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),    
-    new Recipe ('A notherTest recipe', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+    new Recipe (
+  'A Test recipe',
+  'This is simply a test',
+  'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+  [
+    new Ingredient ('Meat', 1),
+    new Ingredient ( 'French Fries', 20)
+  ]  ),  
+    new Recipe (
+  'A notherTest recipe', 
+  'This is simply a test', 
+  'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+  [
+    new Ingredient ('Burger', 3),
+    new Ingredient ( 'Meat', 20)
+  ])
   ];
   constructor() { }
 
